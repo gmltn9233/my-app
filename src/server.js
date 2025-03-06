99dd17b8ef2f47526a210dda09ef6daf87a95605
이희수 <hslee9234@naver.com>
2025-03-06 15:30:50 +0900

    Update server.js

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello, CI/CD with Docker and GitHub Actions!");
+  res.send("Hello, CI/CD completed!");
 });
 
 const PORT = 80;
